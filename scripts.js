227b7d5062fe9c419e1d85b736694a98f79ef12b
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2023-05-14 12:37:08 +0200

    Getting started
    added requirments to readme

---
+++ scripts.js
@@ -0,0 +1,8 @@
+{
+    Ashwin: 'Western Cape',
+      Sibongile: 'Gauteng',
+    'Jan-Hendrik': 'Northern Cape',
+      Sifso: 'Eastern Cape',
+      Shailen: 'KwaZulu-Natal',
+      Frikkie: 'Free State',
+}
